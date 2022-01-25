f067b387b1b3b5eabac82f4f9bc864c4167b9f65
Mariana Viana Rodriguez <workdeveloper.new@gmail.com>
2022-01-25 11:54:51 +0100

    update bonus

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
   const [adalabers, setAdalabers] = useState([]);
   //aqui guardo el value
   const [search, setSearch] = useState("");
+  const [filterCounselor, setfilterCounselor] = useState("All");
   const [newAdalaber, setNewAdalaber] = useState({
     name: "",
     counselor: "",
@@ -13,7 +14,7 @@ function App() {
   });
 
   useEffect(() => {
-    getAdalabers().then((data) => {
+    getAdalabers().then(data => {
       setAdalabers(data.results);
     });
   }, []);
@@ -25,8 +26,13 @@ function App() {
     });
   };
 
+  const handleChangeCounselor = (eve) => {
+    setfilterCounselor(eve.currentTarget.value);
+  };
+
   const handleClick = (eve) => {
     eve.preventDefault();
+    newAdalaber.id = adalabers.length;
     setAdalabers([...adalabers, newAdalaber]);
     setNewAdalaber({
       name: "",
@@ -34,25 +40,37 @@ function App() {
       speciality: "",
     });
   };
+ 
+  const isDisabled= newAdalaber === "";
 
   const handleSearchAdalaber = (eve) => {
-    const currentValue = eve.currentTarget.value;
-    setSearch(currentValue);
+    const current = eve.currentTarget.value;
+    setSearch(current);
   };
 
-  const renderListAdalabers = adalabers.filter((student) =>
-    student.name.toLowerCase().includes(search.toLowerCase())
-  )
-  .map((student,index) => {
-    return (
-      <tr key={index}>
-        <td>{student.name}</td>
-        <td>{student.counselor}</td>
-        <td>{student.speciality}</td>
-      </tr>
-    );
-  }
-  );
+  const renderListAdalabers = adalabers
+    .filter((student) =>
+      student.name.toLowerCase().includes(search.toLowerCase())
+    )
+    .filter(
+      (eachAdalaber) =>
+        filterCounselor === "All" || filterCounselor === eachAdalaber.counselor
+    )
+    .map((student, index) => {
+      return (
+        <tr key={index} id={student.id}>
+          <td>{student.name}</td>
+          <td>{student.counselor}</td>
+          <td>{student.speciality}</td>
+          <th>
+            <ul>{student.social_networks.map(item => {
+            return (
+              <li><a href={item.url}>{item.name}</a></li>
+            )
+          })}</ul></th>
+        </tr>
+      );
+    });
 
   return (
     <>
@@ -61,22 +79,37 @@ function App() {
       </header>
       <main>
         <form className="form">
-          <label htmlFor="search" className="form__label">Nombre:</label>
+          <label htmlFor="search" className="form__label">
+            Nombre:
+          </label>
           <input
-          className="form__input"
-          autoComplete="off"
+            className="form__input"
+            autoComplete="off"
             type="text"
             name="search"
             placeholder="filtrar alumna"
             onChange={handleSearchAdalaber}
           />
+          <label htmlFor="counselor">Escoje una Tutora</label>
+          <select
+            name="counselor"
+            id="counselor"
+            onChange={handleChangeCounselor}
+            value={filterCounselor}
+          >
+            <option value="All">Cualquiera</option>
+            <option Value="Yanelis">Yanelis</option>
+            <option value="Dayana">Dayana</option>
+            <option value="Iván">Ivan</option>
+          </select>
         </form>
-        <table  className="table" cellPadding="2">
+        <table className="table" cellPadding="2">
           <thead>
             <tr>
               <th>Nombre</th>
               <th>Tutora</th>
               <th>Especialidad</th>
+              <th>Redes Sociales</th>
             </tr>
           </thead>
           <tbody>{renderListAdalabers}</tbody>
@@ -84,41 +117,48 @@ function App() {
 
         <form className="add-form">
           <h2 className="add-form__h2">Añadir una Adalaber</h2>
-          <label htmlFor="name" className="add-form__label">Nombre</label>
+          <label htmlFor="name" className="add-form__label">
+            Nombre
+          </label>
           <input
-          className="add-form__input"
+            className="add-form__input"
             type="text"
             name="name"
             id="name"
             placeholder="nombre"
-            value={adalabers.name}
+            value={newAdalaber.name}
             onChange={handleAddNewAdalaber}
           />
-          <label htmlFor="counselor" className="add-form__label">Tutora</label>
+          <label htmlFor="counselor" className="add-form__label">
+            Tutora
+          </label>
           <input
-          className="add-form__input"
+            className="add-form__input"
             type="text"
             name="counselor"
             id="counselor"
             placeholder="Tutora"
-            value={adalabers.counselor}
+            value={newAdalaber.counselor}
             onChange={handleAddNewAdalaber}
           />
-          <label htmlFor="speciality" className="add-form__label">Especialidad</label>
+          <label htmlFor="speciality" className="add-form__label">
+            Especialidad
+          </label>
           <input
-          className="add-form__input"
+            className="add-form__input"
             type="text"
             name="speciality"
             id="speciality"
             placeholder="especialidad"
-            value={adalabers.speciality}
+            value={newAdalaber.speciality}
             onChange={handleAddNewAdalaber}
           />
           <input
-          className="add-form__btn"
+            className="add-form__btn"
             type="submit"
             value="Añadir una nueva Adalaber"
             onClick={handleClick}
+            disabled={isDisabled}
           />
         </form>
       </main>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 const getAdalabers = () => {
   return fetch(
     "https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json"
-  ).then((response) => response.json());
+  )
+    .then((response) => response.json());
 };
 
 export default getAdalabers;
